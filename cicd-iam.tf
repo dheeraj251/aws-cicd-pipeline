b318803d2850c424e88cfb3a94e27da8bbefe328
Dheeraj Pal <dheeraj251@gmail.com>
2021-04-06 22:08:18 +0530

    added iam policy acces

--- cicd-iam.tf
+++ cicd-iam.tf
@@ -72,7 +72,7 @@ EOF
 data "aws_iam_policy_document" "tf-cicd-build-policies" {
   statement {
     sid       = ""
-    actions   = ["logs:*", "s3:*", "codebuild:*", "secretsmanager:*"]
+    actions   = ["logs:*", "s3:*", "codebuild:*", "secretsmanager:*", "iam:*"]
     resources = ["*"]
     effect    = "Allow"
   }
